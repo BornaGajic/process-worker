2939b0752382d866154ec76c0b3402ad31de1d72
Borna Gajic <bgajic@acadiangroup.com>
2023-11-09 19:49:04 +0100

    V2 update

--- ProcessWorkerExample/Specs/ProcessWorkerV2Tests.cs
+++ ProcessWorkerExample/Specs/ProcessWorkerV2Tests.cs
@@ -22,34 +22,67 @@ namespace Specs
 
             _logger = Container.Resolve<ILogger>();
 
-            _processWorkerST = processWorkerProvider.GetOrCreate("ProcessWorkerTest_Sync", new ProcessWorkerConfiguration
+            _processWorkerST = processWorkerProvider.GetOrCreate($"ST-{nameof(ProcessWorkerV2Tests)}", new ProcessWorkerConfiguration
             {
                 Concurrency = 1
             });
 
-            _processWorkerMT = processWorkerProvider.GetOrCreate("ProcessWorkerTest_Async", new ProcessWorkerConfiguration
-            {
-                Concurrency = 3
-            });
+            _processWorkerMT = processWorkerProvider.GetOrCreate($"MT-{nameof(ProcessWorkerV2Tests)}");
         }
 
         private IContainer Container { get; }
 
-        [Fact(DisplayName = "T01: Sum integers")]
-        public async Task T01_SumFirstNIntegers()
+        [Fact(DisplayName = $"T01: MT-{nameof(ProcessWorker)}")]
+        public async Task T01()
         {
             int numOfIntegers = 10;
             var expectedResult = Enumerable.Range(1, numOfIntegers).Sum(i => i); // 55
 
-            _logger.Information($"Start {nameof(SumFirstNIntegers)} with {nameof(numOfIntegers)} set to {numOfIntegers}.");
-
             var sumIntegersMtTest = await SumFirstNIntegers(numOfIntegers, useParallel: true);
-            var sumIntegersStTest = await SumFirstNIntegers(numOfIntegers, useParallel: false);
-
-            _logger.Information($"End {nameof(SumFirstNIntegers)}");
 
             sumIntegersMtTest.Should().Be(expectedResult);
+        }
+
+        [Fact(DisplayName = $"T02: ST-{nameof(ProcessWorker)}")]
+        public async Task T02()
+        {
+            int numOfIntegers = 10;
+            var expectedResult = Enumerable.Range(1, numOfIntegers).Sum(i => i); // 55
+
+            var sumIntegersStTest = await SumFirstNIntegers(numOfIntegers, useParallel: false);
+
             sumIntegersStTest.Should().Be(expectedResult);
+        }
+
+        [Fact(DisplayName = $"T03: Cancel {nameof(ProcessWorker)}")]
+        public async Task T03()
+        {
+            var info = await _processWorkerST.EnqueueWorkItemAsync(async cancel =>
+            {
+                foreach (var item in Enumerable.Range(1, 20))
+                {
+                    cancel.ThrowIfCancellationRequested();
+                    await Task.Delay(500);
+                }
+            });
+
+            await Task.Delay(1_000);
+
+            _processWorkerST.CancelWorkItemAsync(info.ProcessId, 250);
+
+            var threwOperationCanceledException = false;
+
+            try
+            {
+                await info.Completion;
+            }
+            catch (Exception ex)
+            {
+                ex.Should().BeAssignableTo<OperationCanceledException>();
+                threwOperationCanceledException = true;
+            }
+
+            threwOperationCanceledException.Should().BeTrue();
         }
 
         private async Task<int> SumFirstNIntegers(int numOfIntegers, bool useParallel)
@@ -68,8 +101,10 @@ namespace Specs
 
                     state.Status = newStatus;
 
-                    if (newStatus is ProcessStatus.Queued)
+                    if (newStatus is ProcessStatus.Running)
                         state.StartTime = DateTime.Now;
+                    else if (newStatus is ProcessStatus.Queued)
+                        state.QueuedAt = DateTime.Now;
                     else if (newStatus is ProcessStatus.Done or ProcessStatus.Canceled or ProcessStatus.Failed)
                         state.EndTime = DateTime.Now;
 
